43ee62ba57b688340ad1b4cf6abd9cd66789460a
João Geonizeli <joao.geonizeli@gmail.com>
2020-12-19 15:25:27 -0300

    add MyContext and ContextFactory

---
+++ api/Api.Data/Context/ContextFactory.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Api.Data.Context
+{
+    public class ContextFactory : IDesignTimeDbContextFactory<MyContext>
+    {
+        public MyContext CreateDbContext(string[] args)
+        {
+            var connectionString = "Host=localhost;Port=5432;Database=scheduler";
+            var optionsBuilder = new DbContextOptionsBuilder<MyContext>();
+            optionsBuilder.UseNpgsql(connectionString);
+
+            return new MyContext(optionsBuilder.Options);
+        }
+    }
+}
