48765b13e728005638e6843f15885d0bab904f3c
João Geonizeli <joao.geonizeli@gmail.com>
2020-12-19 21:24:52 -0300

    implement login controller

---
+++ api/Api.Application/Controllers/LoginController.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Net;
+using System.Threading.Tasks;
+using Api.Domain.Entities;
+using Api.Domain.Interfaces.Services.User;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Application.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<object> Login([FromBody] UserEntity user, [FromServices] ILoginService service)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (user == null)
+            {
+                return BadRequest();
+            }
+
+            try
+            {
+                var result = await service.FindByLoginAsync(user);
+
+                if (result != null)
+                {
+                    return Ok(result);
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            catch (ArgumentException e)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+    }
+}
